d6b708dfa9a006fe2404c8697e1d2844af49d2bd
muhridwanf12 <80451068+muhridwanf12@users.noreply.github.com>
2024-04-14 22:00:53 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,53 @@ hamberger.addEventListener('click', () =>{
 })
 
 
+// Validasi form 
+$(document).ready(function(){
+    $('#form').validate({
+        rules:{
+            nama:{
+                minlength: 2
+            },
+            email:{
+                email: true,
+                required: true
+            },
+            number: {
+                number: true,
+                required: true,
+                minlength: 9,
+                maxlength: 15
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            nama:{
+                required: "Wajib diisi",
+                minlength: "Nama paling sedikit terdiri dari 2 huruf"
+            },
+            email:{
+                email: "Isi email dengan benar",
+                required: "Wajib diisi"
+            },
+            number: {
+                number: "Isi nomor dengan benar",
+                required: "Wajib diisi",
+                minlength: "Nomor handphone minimal 8 angka",
+                maxlength: "Nomor handphone maksimal 15 angka"
+            },
+            message: {
+                required: "Wajib diisi"
+            },
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
+
+
 
 
 
